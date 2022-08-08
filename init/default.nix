9297beae6e2b651500738ab5ef58c86d59cec82c
Paul Gray <paul.gray@instructure.com>
2022-08-08 15:16:51 -0400

    Fixing deps of init scripts

--- init/default.nix
+++ init/default.nix
@@ -3,10 +3,24 @@ system: {pkgs}:
     name = "init";
     src = ./.;
 
-    buildInputs = [pkgs.envsubst];
+    buildInputs = [
+      pkgs.envsubst
+      pkgs.makeWrapper
+      pkgs.nix
+      pkgs.coreutils
+    ];
 
     installPhase = ''
       mkdir $out
       cp -rv $src/* $out
+    '';
+
+    postFixup = ''
+      wrapProgram $out/bin/init \
+        --set PATH ${pkgs.lib.makeBinPath [
+          pkgs.envsubst
+          pkgs.nix
+          pkgs.coreutils
+        ]}
     '';
   }
