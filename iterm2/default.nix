0b50c382976e4b6f8fe102fc2dd8ff1a352854eb
Paul Gray <paul.gray@instructure.com>
2024-01-19 09:25:04 -0500

    iterm2 profiles

--- iterm2/default.nix
+++ iterm2/default.nix
@@ -3,4 +3,7 @@
 lib.mkIf pkgs.stdenv.isDarwin {
   home.file."Library/Application\ Support/iTerm2/DynamicProfiles/iterm-profiles.json".source =
      ./iterm-profiles.json;
+
+  #home.file."Library/Application\ Support/iTerm2/Scripts/AutoLaunch/set-default-profile.py".source =
+   #  ./set-default-profile.py;
 }
