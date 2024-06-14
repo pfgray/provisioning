a9a16ca756985ebf064578648332d37fc5193001
Paul Gray <paul.gray@instructure.com>
2024-06-14 09:40:39 -0400

    Adding gamesheet stuff

--- video-tools/gamesheet/processGamesheet.sh
+++ video-tools/gamesheet/processGamesheet.sh
@@ -3,27 +3,40 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-output=""
+contents=""
+
 
 if [ -z "${1-}" ]; then
-  echo "no gamesheet specified"
-  echo "usage: process-gamesheet <gamesheel.html>"
-  exit 1;
+  contents=$(cat -)
+else
+  contents=$(cat "${1-}")
 fi
 
-contents=$(cat "${1-}")
+home_team_name=$(echo "$contents" | sed -nr 's/var HomeTeam = "(.*)";/\1/p' | awk '{$1=$1};1')
+home_team_id=$(echo "$contents" | sed -nr 's/var HomeId = "(.*)";/\1/p' | awk '{$1=$1};1')
+away_team_name=$(echo "$contents" | sed -nr 's/var AwayTeam = "(.*)";/\1/p' | awk '{$1=$1};1')
+away_team_id=$(echo "$contents" | sed -nr 's/var AwayId = "(.*)";/\1/p' | awk '{$1=$1};1')
 
+game_stats=$(echo "$contents" | sed -nr 's/var db_Elements = (.*);/\1/p' | awk '{$1=$1};1')
 
-home_team_name=$(sed -nr 's/var HomeTeam = "(.*)";/\1/p' gamesheet.html | awk '{$1=$1};1')
-home_team_id=$(sed -nr 's/var HomeId = "(.*)";/\1/p' gamesheet.html | awk '{$1=$1};1')
+goals=$(echo "$game_stats" | jq -r '.[] | select(.type == "goal") | .team')
 
-away_team=$(sed -nr 's/var AwayTeam = "(.*)";/\1/p' gamesheet.html | awk '{$1=$1};1')
-away_team_id=$(sed -nr 's/var AwayId = "(.*)";/\1/p' gamesheet.html | awk '{$1=$1};1')
+home_team_goals=0
+away_team_goals=0
 
-
-game_stats=$(sed -nr 's/var db_Elements = (.*);/\1/p' gamesheet.html | awk '{$1=$1};1')
-
-
+for goal in $goals; do
+  if [ "$goal" == "$home_team_id" ]; then
+    home_team_goals=$((home_team_goals + 1))
+    echo "$away_team_name $away_team_goals"
+    echo "$home_team_name $home_team_goals"
+    echo "----"
+  else
+    away_team_goals=$((away_team_goals + 1))
+    echo "$away_team_name $away_team_goals"
+    echo "$home_team_name $home_team_goals"
+    echo "----"
+  fi
+done
 
 
 
